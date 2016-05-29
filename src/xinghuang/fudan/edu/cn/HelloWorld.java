5c2487c4ec751275b53ee858fa36e820eedd5b6d
XingHuang <hxsace@163.com>
2016-05-29 15:20:10 +0800

    add date

--- src/xinghuang/fudan/edu/cn/HelloWorld.java
+++ src/xinghuang/fudan/edu/cn/HelloWorld.java
@@ -1,13 +1,17 @@
 package xinghuang.fudan.edu.cn;
 
+import java.util.Date;
+
 /**
  * Created by XingHuang on 5/22/16.
  */
 public class HelloWorld {
 
     public static  void  main(String[] args){
+        Date d = new Date(20160529);
         String text = "Hello World";
         System.out.print(text);
+
 
     }
 
