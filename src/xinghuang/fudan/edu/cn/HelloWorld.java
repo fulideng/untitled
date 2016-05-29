0ab85b19ddc97c8b8d3e8bb7d27c9a23b9bb8e48
XingHuang <XingHuang@XingHuangs-MacBook-Pro.local>
2016-05-29 14:05:17 +0800

    Initial commit

---
+++ src/xinghuang/fudan/edu/cn/HelloWorld.java
@@ -0,0 +1,12 @@
+package xinghuang.fudan.edu.cn;
+
+/**
+ * Created by XingHuang on 5/22/16.
+ */
+public class HelloWorld {
+
+    public static  void  main(String[] args){
+        System.out.print("hello world!");
+    }
+
+}
